07b4cbc9885459aa085580374663cceea91fd1a1
Yan Lin <lambertlin@outlook.com>
2021-03-22 15:21:25 -0400

    add prettier eslint

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Initialize middleware
+app.use(express.json());
+
+// Listening on port 3000 by default
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
