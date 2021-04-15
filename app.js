e64ef5239f81a39c8993d45369124fcf64f6c0ab
Yan Lin <lambertlin@outlook.com>
2021-04-15 15:23:32 -0400

    structured project folder

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
 import express from "express";
-import session from "express-session";
 import config from "config";
-import mongoose from "mongoose";
-import MongoStore from "connect-mongo";
+import router from "./lib/routes";
+import sessionStore from "./lib/db/sessionStore";
 
 const app = express();
 const PORT = config.get("app.port");
-const dbUri = config.get("database.uri");
-
-mongoose.connect(dbUri, {
-  dbName: config.get("database.dbName"),
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-// Get the default connection
-const { connection } = mongoose;
-
-// Bind connection to error event (to get notification of connection errors)
-connection.on(
-  "error",
-  console.error.bind(console, "MongoDB connection error:")
-);
-
-connection.once(
-  "open",
-  console.log.bind("Connection established successfully!")
-);
 
 // Initialize middleware
 app.use(express.json());
@@ -36,28 +13,8 @@ app.use(
     extended: true,
   })
 );
-
-app.use(
-  session({
-    secret: config.get("session.secret"),
-    resave: false,
-    saveUninitialized: false,
-    // cookie: config.get("session.cookie"),
-    store: MongoStore.create({
-      // mongoUrl: dbUri,
-      client: connection.getClient(),
-      dbName: config.get("database.dbName"),
-      collectionName: "sessions",
-    }),
-  })
-);
-
-app.get("/", (req, res) => {
-  req.session.isAuth = true;
-  console.log(req.session);
-  console.log(req.session.id);
-  res.send("Hello World");
-});
+app.use(sessionStore);
+app.use(router);
 
 // Listening on port 3000 by default
 app.listen(PORT, console.log(`Server is listening on PORT ${PORT}`));
